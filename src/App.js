c3e80e0e919d76119c619cc1c35d23937f7399cf
Enio Nicael <enionicael@gmail.com>
2021-09-21 10:53:11 -0300

    Feat: Creation of component signatures

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Header from './components/Header';
+import AboutMe from './components/AboutMe';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Enio Nicael</h1>
+    <div className="container">
+      <Header />
+      <AboutMe />
+      <Skills />
+      <Projects />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Header = () => (
+  <header className="header">
+    <div className="introduce">
+      <h1>Enio Nicael</h1>
+      <h2>Frontend Dev</h2>
+    </div>
+    <Navbar />
+  </header>
+);
+
+export default Header;
